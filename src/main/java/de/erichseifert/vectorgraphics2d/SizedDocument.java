add64800a3fb668a04ea54fa5aa09c6756ceae8a
Erich Seifert <dev@erichseifert.de>
2013-12-22 13:20:28 +0100

    Added base classes for documents.

---
+++ src/main/java/de/erichseifert/vectorgraphics2d/SizedDocument.java
@@ -0,0 +1,40 @@
+/*
+ * VectorGraphics2D: Vector export for Java(R) Graphics2D
+ *
+ * (C) Copyright 2010-2013 Erich Seifert <dev[at]erichseifert.de>
+ *
+ * This file is part of VectorGraphics2D.
+ *
+ * VectorGraphics2D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VectorGraphics2D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with VectorGraphics2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.erichseifert.vectorgraphics2d;
+
+import de.erichseifert.vectorgraphics2d.util.PageSize;
+
+public abstract class SizedDocument implements Document {
+	private final PageSize pageSize;
+
+	public SizedDocument(PageSize pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public PageSize getPageSize() {
+		return pageSize;
+	}
+
+	public void close() {
+	}
+}
+
