e5b99c3ed855f622abd2374a1ef3a0fac452f112
Erich Seifert <dev@erichseifert.de>
2013-12-28 19:14:44 +0100

    Resolve ambiguous import of List.

--- src/main/java/de/erichseifert/vectorgraphics2d/pdf/PDFDocument.java
+++ src/main/java/de/erichseifert/vectorgraphics2d/pdf/PDFDocument.java
@@ -38,6 +38,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
+import java.util.List;
 
 /**
  * @author Erich Seifert
