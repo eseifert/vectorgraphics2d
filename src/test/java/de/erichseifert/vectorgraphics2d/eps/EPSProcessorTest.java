3aeb49a8dbea82cce9df21b0de727844789f988b
Michael Seifert <mseifert@error-reports.org>
2015-02-19 19:50:07 +0100

    Updated EPSProcessorTest to reflect recent changes.

--- src/test/java/de/erichseifert/vectorgraphics2d/eps/EPSProcessorTest.java
+++ src/test/java/de/erichseifert/vectorgraphics2d/eps/EPSProcessorTest.java
@@ -20,18 +20,18 @@
  */
 package de.erichseifert.vectorgraphics2d.eps;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import static de.erichseifert.vectorgraphics2d.TestUtils.Template;
+import static de.erichseifert.vectorgraphics2d.TestUtils.assertTemplateEquals;
+
 import de.erichseifert.vectorgraphics2d.Document;
 import de.erichseifert.vectorgraphics2d.intermediate.Command;
 import de.erichseifert.vectorgraphics2d.intermediate.CommandStream;
 import de.erichseifert.vectorgraphics2d.util.PageSize;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.regex.Pattern;
-
-import static de.erichseifert.vectorgraphics2d.TestUtils.assertTemplateEquals;
-import static de.erichseifert.vectorgraphics2d.TestUtils.Template;
 
 public class EPSProcessorTest {
 	private static final String EOL = "\n";
@@ -41,6 +41,7 @@ public class EPSProcessorTest {
 		"%%HiResBoundingBox: 0.0 28.34645669291339 56.69291338582678 113.38582677165356",
 		"%%LanguageLevel: 3",
 		"%%Pages: 1",
+		"%%EndComments",
 		"%%Page: 1 1",
 		"/M /moveto load def",
 		"/L /lineto load def",
@@ -50,7 +51,8 @@ public class EPSProcessorTest {
 		"/rgb /setrgbcolor load def",
 		"/rect { /height exch def /width exch def /y exch def /x exch def x y M width 0 RL 0 height RL width neg 0 RL } bind def",
 		"/ellipse { /endangle exch def /startangle exch def /ry exch def /rx exch def /y exch def /x exch def /savematrix matrix currentmatrix def x y translate rx ry scale 0 0 1 startangle endangle arcn savematrix setmatrix } bind def",
-		"/imgdict { /datastream exch def /hasdata exch def /decodeScale exch def /bits exch def /bands exch def /imgheight exch def /imgwidth exch def << /ImageType 1 /Width imgwidth /Height imgheight /BitsPerComponent bits /Decode [bands {0 decodeScale} repeat] /ImageMatrix [imgwidth 0 0 imgheight 0 0] hasdata { /DataSource datastream } if >> } bind def",
+		"/imgdict { /datastream exch def /hasdata exch def /decodeScale exch def /bits exch def /bands exch def /imgheight exch def /imgwidth exch def << /ImageType 1 /Width imgwidth /Height imgheight /BitsPerComponent bits /Decode [bands {0 decodeScale} repeat]",
+		" /ImageMatrix [imgwidth 0 0 imgheight 0 0] hasdata { /DataSource datastream } if >> } bind def",
 		"/latinize { /fontName exch def /fontNameNew exch def fontName findfont 0 dict copy begin /Encoding ISOLatin1Encoding def fontNameNew /FontName def currentdict end dup /FID undef fontNameNew exch definefont pop } bind def",
 		Pattern.compile("/\\S+?Lat /\\S+ latinize /\\S+?Lat 12.0 selectfont"),
 		"gsave",
@@ -59,7 +61,7 @@ public class EPSProcessorTest {
 		"0 85.03937007874016 translate",
 		"2.834645669291339 -2.834645669291339 scale",
 		"/basematrix matrix currentmatrix def",
-		"1.0 1.0 1.0 rgb"
+		"%%EOF"
 	};
 	private static final PageSize PAGE_SIZE = new PageSize(0.0, 10.0, 20.0, 30.0);
 
