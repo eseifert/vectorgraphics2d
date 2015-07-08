f16411d04cfe000f475163058d2e64048b669fc4
Michael Seifert <mseifert@error-reports.org>
2015-07-08 11:14:39 +0200

    CharacterTest shows all characters of the ISO-8859-1 charset.

--- src/test/java/de/erichseifert/vectorgraphics2d/CharacterTest.java
+++ src/test/java/de/erichseifert/vectorgraphics2d/CharacterTest.java
@@ -22,6 +22,10 @@ package de.erichseifert.vectorgraphics2d;
 
 import java.awt.Graphics2D;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CharacterTest extends TestCase {
 	public CharacterTest() throws IOException {
@@ -32,19 +36,27 @@ public class CharacterTest extends TestCase {
 		double w = getPageSize().width;
 		double h = getPageSize().height;
 
-		// TODO: Test complete charsets
-		char[] characters = new char[] {'ä', 'ö', 'å'};
-		final int colCount = 3;
-		final int rowCount = 1;
+		Charset latin1 = Charset.forName("ISO-8859-1");
+		CharsetEncoder latin1Encoder = latin1.newEncoder();
+		List<String> charactersInCharset = new ArrayList<String>();
+		for (char char_ = Character.MIN_VALUE; char_ < Character.MAX_VALUE; char_++) {
+			String javaString = String.valueOf(char_);
+			if (latin1Encoder.canEncode(char_)) {
+				charactersInCharset.add(javaString);
+			}
+		}
+
+		final int colCount = (int) Math.ceil(Math.sqrt(charactersInCharset.size()));
+		final int rowCount = colCount;
 		double tileWidth = w/colCount;
 		double tileHeight = h/rowCount;
 		int charIndex = 0;
 		for (double y = 0.0; y < h; y += tileHeight) {
 			for (double x = 0.0; x < w; x += tileWidth) {
-				char c = characters[charIndex];
+				String c = charactersInCharset.get(charIndex);
 				double tileCenterX = x + tileWidth/2.0;
 				double tileCenterY = y + tileHeight/2.0;
-				g.drawString(String.valueOf(c), (float) tileCenterX, (float) tileCenterY);
+				g.drawString(c, (float) tileCenterX, (float) tileCenterY);
 				charIndex++;
 			}
 		}
