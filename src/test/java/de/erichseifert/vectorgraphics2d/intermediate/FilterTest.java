c362a4d1ae8519b5db1ac71c4d19f35fdb8ad91b
Michael Seifert <mseifert@error-reports.org>
2016-01-04 00:54:48 +0100

    FilterTest uses a List to store Command objects instead of a CommandStream.

--- src/test/java/de/erichseifert/vectorgraphics2d/intermediate/FilterTest.java
+++ src/test/java/de/erichseifert/vectorgraphics2d/intermediate/FilterTest.java
@@ -30,6 +30,7 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
@@ -41,12 +42,12 @@ import de.erichseifert.vectorgraphics2d.intermediate.commands.SetTransformComman
 
 public class FilterTest {
 	@Test public void filterNone() {
-		CommandStream stream = new CommandStream();
-		stream.add(null, new SetColorCommand(Color.BLACK));
-		stream.add(null, new SetStrokeCommand(new BasicStroke(1f)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
-		stream.add(null, new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
+		List<Command<?>> stream = new LinkedList<Command<?>>();
+		stream.add(new SetColorCommand(Color.BLACK));
+		stream.add(new SetStrokeCommand(new BasicStroke(1f)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
+		stream.add(new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
 
 		Iterator<Command<?>> unfiltered = stream.iterator();
 
@@ -65,12 +66,12 @@ public class FilterTest {
 	}
 
 	@Test public void filterAll() {
-		CommandStream stream = new CommandStream();
-		stream.add(null, new SetColorCommand(Color.BLACK));
-		stream.add(null, new SetStrokeCommand(new BasicStroke(1f)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
-		stream.add(null, new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
+		List<Command<?>> stream = new LinkedList<Command<?>>();
+		stream.add(new SetColorCommand(Color.BLACK));
+		stream.add(new SetStrokeCommand(new BasicStroke(1f)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
+		stream.add(new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
 
 		Iterator<Command<?>> unfiltered = stream.iterator();
 
@@ -86,12 +87,12 @@ public class FilterTest {
 	}
 
 	@Test public void duplicate() {
-		CommandStream stream = new CommandStream();
-		stream.add(null, new SetColorCommand(Color.BLACK));
-		stream.add(null, new SetStrokeCommand(new BasicStroke(1f)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
-		stream.add(null, new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
-		stream.add(null, new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
+		List<Command<?>> stream = new LinkedList<Command<?>>();
+		stream.add(new SetColorCommand(Color.BLACK));
+		stream.add(new SetStrokeCommand(new BasicStroke(1f)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 10.0, 11.0)));
+		stream.add(new SetTransformCommand(AffineTransform.getTranslateInstance(5.0, 5.0)));
+		stream.add(new DrawShapeCommand(new Line2D.Double(0.0, 1.0, 5.0, 6.0)));
 
 		Iterator<Command<?>> unfiltered = stream.iterator();
 
