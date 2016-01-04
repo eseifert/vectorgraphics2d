e45ba242d9f3bb2923afce9c7efe4c515a370c4d
Michael Seifert <mseifert@error-reports.org>
2016-01-04 19:44:03 +0100

    Added a skeleton for a filter class that replaces DrawShapeCommands with DrawImageCommands, if the shape uses a Paint.

---
+++ src/main/java/de/erichseifert/vectorgraphics2d/intermediate/filters/DrawPaintedShapeAsImageFilter.java
@@ -0,0 +1,38 @@
+/*
+ * VectorGraphics2D: Vector export for Java(R) Graphics2D
+ *
+ * (C) Copyright 2010-2016 Erich Seifert <dev[at]erichseifert.de>
+ *
+ * This file is part of VectorGraphics2D.
+ *
+ * VectorGraphics2D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VectorGraphics2D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with VectorGraphics2D.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.erichseifert.vectorgraphics2d.intermediate.filters;
+
+import java.util.List;
+
+import de.erichseifert.vectorgraphics2d.intermediate.Command;
+
+public class DrawPaintedShapeAsImageFilter extends Filter {
+
+	public DrawPaintedShapeAsImageFilter(Iterable<Command<?>> stream) {
+		super(stream);
+	}
+
+	@Override
+	protected List<Command<?>> filter(Command<?> command) {
+		return null;
+	}
+}
+
