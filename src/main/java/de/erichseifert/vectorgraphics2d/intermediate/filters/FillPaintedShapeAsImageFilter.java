d213ab4faeabe93d86eaf44f7ea3a25be7b6d8ac
Michael Seifert <mseifert@error-reports.org>
2016-01-04 22:45:45 +0100

    Implemented FillPaintedShapeAsImageFilter.

--- src/main/java/de/erichseifert/vectorgraphics2d/intermediate/filters/FillPaintedShapeAsImageFilter.java
+++ src/main/java/de/erichseifert/vectorgraphics2d/intermediate/filters/FillPaintedShapeAsImageFilter.java
@@ -20,19 +20,71 @@
  */
 package de.erichseifert.vectorgraphics2d.intermediate.filters;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.List;
 
 import de.erichseifert.vectorgraphics2d.intermediate.Command;
+import de.erichseifert.vectorgraphics2d.intermediate.commands.DisposeCommand;
+import de.erichseifert.vectorgraphics2d.intermediate.commands.DrawImageCommand;
+import de.erichseifert.vectorgraphics2d.intermediate.commands.FillShapeCommand;
+import de.erichseifert.vectorgraphics2d.intermediate.commands.SetPaintCommand;
 
 public class FillPaintedShapeAsImageFilter extends Filter {
+	private SetPaintCommand lastSetPaintCommand;
 
 	public FillPaintedShapeAsImageFilter(Iterable<Command<?>> stream) {
 		super(stream);
 	}
 
 	@Override
+	public Command<?> next() {
+		Command<?> nextCommand = super.next();
+
+		if (nextCommand instanceof SetPaintCommand) {
+			lastSetPaintCommand = (SetPaintCommand) nextCommand;
+		} else if (nextCommand instanceof DisposeCommand) {
+			lastSetPaintCommand = null;
+		}
+
+		return nextCommand;
+	}
+
+	private DrawImageCommand getDrawImageCommand(FillShapeCommand shapeCommand, SetPaintCommand paintCommand) {
+		Shape shape = shapeCommand.getValue();
+		Rectangle2D shapeBounds = shape.getBounds2D();
+		double x = shapeBounds.getX();
+		double y = shapeBounds.getY();
+		double width = shapeBounds.getWidth();
+		double height = shapeBounds.getHeight();
+		int imageWidth = (int) Math.round(width);
+		int imageHeight = (int) Math.round(height);
+		BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D imageGraphics = (Graphics2D) image.getGraphics();
+		imageGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		imageGraphics.scale(imageWidth/width, imageHeight/height);
+		imageGraphics.translate(-shapeBounds.getX(), -shapeBounds.getY());
+		imageGraphics.setPaint(paintCommand.getValue());
+		imageGraphics.fill(shape);
+		imageGraphics.dispose();
+
+		DrawImageCommand drawImageCommand = new DrawImageCommand(image, imageWidth, imageHeight, x, y, width, height);
+		return drawImageCommand;
+	}
+
+	@Override
 	protected List<Command<?>> filter(Command<?> command) {
-		return null;
+		if (lastSetPaintCommand != null && command instanceof FillShapeCommand) {
+			FillShapeCommand fillShapeCommand = (FillShapeCommand) command;
+			DrawImageCommand drawImageCommand = getDrawImageCommand(fillShapeCommand, lastSetPaintCommand);
+			return Arrays.<Command<?>>asList(drawImageCommand);
+		}
+
+		return Arrays.<Command<?>>asList(command);
 	}
 }
 
