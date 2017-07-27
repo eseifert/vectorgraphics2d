0d759fcf9e6b5ebe38d00a4aec25b7426ed977b1
Erich Seifert <dev@erichseifert.de>
2017-07-27 14:14:10 +0200

    Updated year in license headers.

--- src/main/java/de/erichseifert/vectorgraphics2d/intermediate/commands/RotateCommand.java
+++ src/main/java/de/erichseifert/vectorgraphics2d/intermediate/commands/RotateCommand.java
@@ -1,7 +1,7 @@
 /*
  * VectorGraphics2D: Vector export for Java(R) Graphics2D
  *
- * (C) Copyright 2010-2016 Erich Seifert <dev[at]erichseifert.de>,
+ * (C) Copyright 2010-2017 Erich Seifert <dev[at]erichseifert.de>,
  * Michael Seifert <mseifert[at]error-reports.org>
  *
  * This file is part of VectorGraphics2D.
