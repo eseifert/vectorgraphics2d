a0a07f04d15f6fc8c98d6d86244ed170c2a8d757
Erich Seifert <dev@erichseifert.de>
2017-08-08 12:59:15 +0200

    Tests: GraphicsUtils.clone(null) returns null.

--- src/test/java/de/erichseifert/vectorgraphics2d/util/GraphicsUtilsTest.java
+++ src/test/java/de/erichseifert/vectorgraphics2d/util/GraphicsUtilsTest.java
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
@@ -294,6 +295,13 @@ public class GraphicsUtilsTest {
 		}) {
 			assertShapeClassIsCloneable(shapeClass);
 		}
+	}
+
+	@Test
+	public void cloneNullReturnsNull() throws InstantiationException, IllegalAccessException {
+		Shape result = GraphicsUtils.clone(null);
+
+		assertNull(result);
 	}
 
 	@Test
